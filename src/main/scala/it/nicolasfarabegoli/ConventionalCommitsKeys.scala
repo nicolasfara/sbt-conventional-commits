28d6dbe198357893abfeedcddf083dfc86916d26
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-05-23 17:51:37 +0200

    feat!: remove fromScript option

--- src/main/scala/it/nicolasfarabegoli/ConventionalCommitsKeys.scala
+++ src/main/scala/it/nicolasfarabegoli/ConventionalCommitsKeys.scala
@@ -3,6 +3,5 @@ package it.nicolasfarabegoli
 import sbt._
 
 trait ConventionalCommitsKeys {
-  val fromScript = settingKey[Option[URL]]("Specify a remote URL for custom script")
   val conventionalCommits = taskKey[Unit]("Task for creating a git hook for enforcing conventional commits")
 }
