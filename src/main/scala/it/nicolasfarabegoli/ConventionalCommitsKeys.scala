79d0c5363f226180b90cbc6185f333b92d47cd7c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-06-19 10:47:15 +0200

    feat: enable warning if no git root is found

--- src/main/scala/it/nicolasfarabegoli/ConventionalCommitsKeys.scala
+++ src/main/scala/it/nicolasfarabegoli/ConventionalCommitsKeys.scala
@@ -4,6 +4,7 @@ import sbt._
 
 trait ConventionalCommitsKeys {
   val conventionalCommits = taskKey[Unit]("Task for creating a git hook for enforcing conventional commits")
+  val warningIfNoGitRoot = settingKey[Boolean]("Raise a warning if no git root is found")
   val scopes = settingKey[Seq[String]]("A list of valid scopes (defaults to any)")
   val types = settingKey[Seq[String]]("A list of valid commit types")
   val successMessage = settingKey[Option[String]]("The message (if any) to show when a commit is valid")
